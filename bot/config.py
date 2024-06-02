4a96094340eb99a78ce37bc0f53f60df149b4f67
thilakeshwar2024 <thilakeshwar2024@outlook.com>
2024-06-02 09:55:43 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ from decouple import config
 try:
     APP_ID = "8440502"
     API_HASH = "e77474ae3075f4000d3418c5a5a3a112"
-    BOT_TOKEN = "5785661511:AAHDvS081-T8ZJYjhf9tX7KIXXA2IwyZn4E"
+    BOT_TOKEN = "5785661511:AAHsfCns9qR_E2QOMHOR_6HtEf2nyNP3Q8s"
     DEV = "2043144248"
     OWNER = [5623055023,2043144248]
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By Op-Leech/Mirror' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
