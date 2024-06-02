5ecf86deebceed13680a30b5fe4e0574021fa6a0
thilakeshwar2024 <thilakeshwar2024@outlook.com>
2024-06-02 10:01:22 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -28,8 +28,6 @@ async def stats(e):
 async def dl_link(event):
     if not event.is_private:
         return
-    if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
-        return
     link, name = "", ""
     try:
         link = event.text.split()[1]
@@ -120,8 +118,6 @@ async def encod(event):
         if not event.is_private:
             return
         event.sender
-        if str(event.sender_id) not in OWNER and event.sender_id !=DEV:
-            return await event.reply("**Sorry You're not An Authorised User!**")
         if not event.media:
             return
         if hasattr(event.media, "document"):
